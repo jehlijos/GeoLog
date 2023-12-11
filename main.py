e5bcd93ef4af83d1343412c93cf6f91215f408fa
Josef Jehlička <117917253+jehlijos@users.noreply.github.com>
2023-12-11 13:39:12 +0100

    zoom on kraje added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import Toplevel, Label
+from tkinter import Toplevel, Label, ttk
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -49,10 +49,16 @@ def loading_screen(root):
     loading_window.geometry("500x200")
     loading_window.iconbitmap("files/ico.ico")
 
-    custom_font = ("Raleway", 48)  # Specify the font family and size
+    custom_font_large = ("Raleway", 48)  # Specify the font family and size for the large text
+    custom_font_small = ("Raleway", 16)  # Specify the font family and size for the small text
 
-    label = Label(loading_window, text="NAČÍTÁNÍ...", font=custom_font)
-    label.pack(pady=20)
+    # Large text label
+    label_large = Label(loading_window, text="NAČÍTÁNÍ...", font=custom_font_large)
+    label_large.pack(pady=10)
+
+    # Small text label
+    label_small = Label(loading_window, text="Dejte si kávičku, za chvíli bude hotovo :)", font=custom_font_small)
+    label_small.pack(pady=10)
 
     return loading_window
 
@@ -91,21 +97,74 @@ def plot_geopackage(root, gpkg_paths, loading_window):
     loading_window.destroy()
 
 
+def plot_geopackage_selection(root, gpkg_paths, loading_window, selected_nazev=None):
+    loading_window = loading_screen(root)
+    root.update()
+
+    fig, ax = plt.subplots()
+
+    # Read GeoPackage files and store them in a dictionary
+    gpkg_data = {}
+    column_mapping = {
+        "geodata/kraje.shp": "nazev",
+        "geodata/okresy.shp": "Název_kra",
+        "geodata/obce_generalized.shp": "nazev_kraj"
+    }
+    # Get the necessary GeoPackage files based on the selected region
+    necessary_gpkg_paths = []
+    for gpkg_path, color in gpkg_paths:
+        if gpkg_path in column_mapping:
+            necessary_gpkg_paths.append((gpkg_path, color))
+
+    for gpkg_path, color in necessary_gpkg_paths:
+        gpkg_data[gpkg_path] = gpd.read_file(gpkg_path)
+
+    # Dictionary mapping shapefile paths to the corresponding column names for filtering
+
+    for gpkg_path, color in necessary_gpkg_paths:
+        gdf = gpkg_data[gpkg_path]
+
+        # Check if the shapefile path is in the column_mapping dictionary
+        if gpkg_path in column_mapping:
+            column_name = column_mapping[gpkg_path]
+
+            # Check if the column exists in the GeoDataFrame
+            if column_name in gdf.columns:
+                # Filter GeoDataFrame based on the selected value in the corresponding column
+                if selected_nazev is not None:
+                    gdf = gdf[gdf[column_name] == selected_nazev]
+
+                # Plot only the borders of the GeoDataFrame on Matplotlib axis and color them
+                gdf.boundary.plot(ax=ax, color=color)
+
+    ax.set_axis_off()
+
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas_widget = canvas.get_tk_widget()
+    canvas_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+    loading_window.destroy()
+
+
 def main():
     print_file_contents("files/console_sign.txt")
     screen_width, screen_height = get_screen_resolution()
     root = tk.Tk()
-    root.geometry(f"{(screen_width - 200)}x{(screen_height - 200)}")
+    root.geometry(f"{(screen_width - 100)}x{(screen_height - 100)}")
     root.title("GeoLog")
     root.iconbitmap("files/ico.ico")
     root.update()
 
     # Specify the paths to your GeoPackage files and their corresponding colors
+    kraje_shp_path = "geodata/kraje.shp"
     gpkg_paths = [
-        ("geodata/kraje.shp", 'purple'),
-        ("geodata/okresy.shp", 'blue'),
+        (kraje_shp_path, 'blue'),
+        ("geodata/okresy.shp", 'green'),
         ("geodata/obce_generalized.shp", 'gray')
     ]
+
+    # Read GeoPackage file using GeoPandas
+    kraje_shp = gpd.read_file(kraje_shp_path)
 
     # Display loading screen
     loading_window = loading_screen(root)
@@ -114,6 +173,49 @@ def main():
     # Plot "kraje.shp" first and then overlay "okresy.shp" on top
     plot_geopackage(root, gpkg_paths[::-1], loading_window)
 
+    ######################### SETTINGS PANEL #########################
+    root2 = tk.Toplevel(root)
+    root2.geometry(f"{(int(screen_width * 0.25))}x{(int(screen_height * 0.45))}")
+    root2.title("GeoLog - nastavení")
+    root2.iconbitmap("files/ico.ico")
+
+    def update_plot(event):
+        selected_nazev = combo_var.get()
+
+        # Check if "Celá ČR" is selected
+        if selected_nazev == "Celá ČR":
+            # Destroy previous canvas widget
+            for widget in root.winfo_children():
+                if isinstance(widget, tk.Widget):
+                    widget.destroy()
+            plot_geopackage(root, gpkg_paths[::-1], loading_window)
+            root.update()
+        else:
+            # Continue with the previous logic for other selections
+            for widget in root.winfo_children():
+                if isinstance(widget, tk.Widget):
+                    widget.destroy()
+            plot_geopackage_selection(root, gpkg_paths[::-1], loading_window, selected_nazev)
+            root.update()
+
+    # kraj selection
+    desc_font = ("Raleway", 10)
+    label = Label(root2, text="Přibliž na kraj:", font=desc_font)
+    label.pack(pady=1)
+
+    combo_var = tk.StringVar(root2)
+    combo_var.set("--vyber kraj--")  # Default text in the combobox
+
+    kraje_nazvy = kraje_shp['nazev'].unique()
+    kraje_nazvy = ['Celá ČR'] + list(kraje_nazvy)
+
+    # Enclose each 'nazev' value in double quotes
+    quoted_kraje_nazvy = ['{}'.format(nazev) for nazev in kraje_nazvy]
+    combo_box = ttk.Combobox(root2, textvariable=combo_var, values=quoted_kraje_nazvy)
+    combo_box.pack(pady=20)
+    combo_box.bind("<<ComboboxSelected>>", update_plot)
+
+    root2.mainloop()
     root.mainloop()
 
 
